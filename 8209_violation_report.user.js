a850d481c34d04bab30e498038fd949f19780622
logroid <yoheiowa+github@gmail.com>
2023-08-13 23:34:03 +0900

    Update 8209_violation_report.user.js

--- 8209_violation_report.user.js
+++ 8209_violation_report.user.js
@@ -19,160 +19,179 @@
 
 (function() {
   'use strict';
-  var pathname = window.location.pathname;
-  console.info(pathname);
-  var limit = 86400000 * 8;
-  var key = 'violation';
-  var violation = {};
 
-  function load() {
-    var v = JSON.parse(GM_getValue(key, '{}'));
-    var vv = {};
-    var now = new Date().getTime();
-    Object.keys(v).forEach(k => {
-      var ts = v[k];
-      if (ts + limit > now) {
-        vv[k] = ts;
-      }
-    });
-    violation = vv;
-  }
-  function save() {
-    GM_setValue(key, JSON.stringify(violation));
-    addReportedCss();
-  }
-  load();
-  function addReportedCss() {
-    var aids = Object.keys(violation);
-    GM_addStyle(
-      aids
-        .map(id => {
-          return 'a[href$="/jp/auction/' + id + '"]';
-        })
-        .join(',') + '{ font-weight:bold; color:red !important; }'
-    );
-    GM_addStyle(
-      aids
-        .map(id => {
-          return 'a[href$="/jp/auction/' + id + '"]:before';
-        })
-        .join(',') +
-        '{ content: "🚨"; display: inline-block; font-size: 30px; }'
-    );
-    GM_addStyle(
-      aids
-        .map(id => {
-          return '.Products__list a[href$="/jp/auction/' + id + '"]:before';
-        })
-        .join(',') +
-        '{ content: "🚨"; display: inline-block; font-size: 50px; margin-top: -70px; position: absolute; }'
-    );
-  }
-  addReportedCss();
-  if (window.location.host == 'page.auctions.yahoo.co.jp') {
-    var $vr = $('a:contains("違反商品の申告")').clone(true);
-    $vr.addClass('violation-report');
-    $('.l-contentsHead').append($vr);
-    GM_addStyle(
-      '.violation-report{ display: block; text-align: center; border-radius: 10px; border: 1px solid red; padding: 10px; color: white !important; background: red; }'
-    );
-    if (/^\/jp\/auction\/(\w+)$/.test(pathname)) {
-      var aid = RegExp.$1;
-      if (violation[aid] != undefined) {
-        GM_addStyle(
-          '.l-contentsHead:before{ content: "🚨通報済み"; display: block; font-size: 30px; text-align: center; }'
-        );
+  // 違反通報クラス
+  class ViolationReport {
+    constructor() {
+      this.limit = 86400000 * 8;
+      this.key = 'violation';
+      this.violation = {};
+      this.load();
+      this.addReportedCss();
+      this.handlePage();
+    }
+
+    // 違反情報のロード
+    load() {
+      const v = JSON.parse(GM_getValue(this.key, '{}'));
+      const now = new Date().getTime();
+      this.violation = Object.keys(v).reduce((acc, k) => {
+        const ts = v[k];
+        if (ts + this.limit > now) {
+          acc[k] = ts;
+        }
+        return acc;
+      }, {});
+    }
+
+    // 違反情報の保存
+    save() {
+      GM_setValue(this.key, JSON.stringify(this.violation));
+      this.addReportedCss();
+    }
+
+    // 違反通報済みのCSS追加
+    addReportedCss() {
+      const aids = Object.keys(this.violation);
+      const createStyle = (selector, style) => {
+        return aids.map(id => selector + id + style).join(',');
+      };
+
+      GM_addStyle(createStyle('a[href$="/jp/auction/', '"]{ font-weight:bold; color:red !important; }'));
+      GM_addStyle(createStyle('a[href$="/jp/auction/', '"]:before{ content: "🚨"; display: inline-block; font-size: 30px; }'));
+      GM_addStyle(createStyle('.Products__list a[href$="/jp/auction/', '"]:before{ content: "🚨"; display: inline-block; font-size: 50px; margin-top: -70px; position: absolute; }'));
+    }
+
+    // ページごとの処理
+    handlePage() {
+      const pathname = window.location.pathname;
+      if (window.location.host === 'page.auctions.yahoo.co.jp') {
+        this.handleProductPage(pathname);
+      } else if (/^\/seller\//.test(pathname)) {
+        this.handleSellerPage();
+      } else {
+        this.handleOtherPages(pathname);
       }
     }
-  } else if (/^\/seller\//.test(pathname)) {
-    var count = 0;
-    var $header = $('.Options__items');
-    $('ul.Products__items li').each((i, li) => {
-      var $li = $(li);
-      var $a = $li.find('.Product__title>a[href*="/jp/auction/"]');
-      if ($a.length > 0) {
-        $li.find('.Product__priceInfo').append(
+
+    // 商品ページの処理
+    handleProductPage(pathname) {
+      const $vr = $('a:contains("違反商品の申告")').clone(true);
+      $vr.addClass('violation-report');
+      $('.l-contentsHead').append($vr);
+      GM_addStyle('.violation-report{ display: block; text-align: center; border-radius: 10px; border: 1px solid red; padding: 10px; color: white !important; background: red; }');
+
+      if (/^\/jp\/auction\/(\w+)$/.test(pathname)) {
+        const aid = RegExp.$1;
+        if (this.violation[aid] !== undefined) {
+          GM_addStyle('.l-contentsHead:before{ content: "🚨通報済み"; display: block; font-size: 30px; text-align: center; }');
+        }
+      }
+    }
+
+    // 販売者ページの処理
+    handleSellerPage() {
+      let count = 0;
+      const $header = $('.Options__items');
+      $('ul.Products__items li').each((i, li) => {
+        const $li = $(li);
+        const $a = $li.find('.Product__title>a[href*="/jp/auction/"]');
+        if ($a.length > 0) {
+          $li.find('.Product__priceInfo').append(
             $('<label class="seller-checkbox">')
               .text('🚨')
-              .append($('<input type="checkbox">').val($a.attr('href'))
-          )
-        );
-        count++;
+              .append($('<input type="checkbox">').val($a.attr('href')))
+          );
+          count++;
+        }
+      });
+
+      if (count > 0) {
+        this.addBulkCheckOption($header);
+        this.addBulkReportButton();
       }
-    });
-    if (count > 0) {
-      if ($header != null) {
-        var $allCheck = $('<label>')
+    }
+
+    // 一括チェックオプションの追加
+    addBulkCheckOption($header) {
+      if ($header !== null) {
+        const $allCheck = $('<label>')
           .text('🚨一括チェック')
           .append($('<input type="checkbox">'));
         $allCheck.click(e => {
-          var $target = $(e.target);
+          const $target = $(e.target);
           $('.seller-checkbox input[type="checkbox"]')
             .prop('checked', $target.prop('checked'))
             .change();
         });
         $header.append($('<li>').append($allCheck));
       }
-      GM_addStyle(
-        '.seller-checkbox input{ margin-left: 10px;  transform: scale(2); } .seller-checkbox { font-size: 20px }'+
-          '.violation_report_all{position: fixed;right: 10px;margin-top: 70px;top: 100px;}' +
-          '.violation_report_all button{font-size: 30px;}' +
-          'li.Product[violation_report="true"]{background-color: #ffbcbc;}'
-      );
+
+      GM_addStyle('.seller-checkbox input{ margin-left: 10px;  transform: scale(2); } .seller-checkbox { font-size: 20px }' +
+        '.violation_report_all{position: fixed;right: 10px;margin-top: 70px;top: 100px;}' +
+        '.violation_report_all button{font-size: 30px;}' +
+        'li.Product[violation_report="true"]{background-color: #ffbcbc;}');
+
       $('.seller-checkbox input[type="checkbox"]').on('click change', e => {
-        var $target = $(e.target),
+        const $target = $(e.target),
           $parent = $target.closest('li.Product');
         $parent.attr('violation_report', $target.prop('checked'));
       });
-      var $button = $('<button>').text('🚨一括通報');
-      $button.click(() => {
-        $('.Products__list input[type="checkbox"]:checked').each(
-          (i_, chk) => {
-            var $chk = $(chk);
-            if ($chk.val().match(/\/auction\/(\w+)$/)) {
-              var aid = RegExp.$1;
-              GM_openInTab(
-                'https://auctions.yahoo.co.jp/jp/show/violation_report?aID=' +
-                  aid,
-                { insert: true }
-              );
-            }
-          }
-        );
-      });
-      $(document.body).append(
-        $('<div>')
-          .addClass('violation_report_all')
-          .append($button)
-      );
     }
-  } else {
-    switch (pathname) {
-      case '/jp/config/violation_report':
-        var href = $(
-          'a:contains("商品ページに戻る"), a:contains("商品詳細ページに戻る")'
-        ).attr('href');
-        if (href.match(/\/auction\/(\w+)$/)) {
-          var aid = RegExp.$1;
-          violation[aid] = new Date().getTime();
-          save();
-        }
-        break;
-      case '/jp/show/violation_report':
-        $('html,body').animate({
-          scrollTop: $('form[action$="/config/violation_report"]').offset().top
+
+    // 一括通報ボタンの追加
+    addBulkReportButton() {
+      const $button = $('<button>').text('🚨一括通報');
+      $button.click(() => {
+        $('.Products__list input[type="checkbox"]:checked').each((i_, chk) => {
+          const $chk = $(chk);
+          if ($chk.val().match(/\/auction\/(\w+)$/)) {
+            const aid = RegExp.$1;
+            GM_openInTab('https://auctions.yahoo.co.jp/jp/show/violation_report?aID=' + aid, { insert: true });
+          }
         });
-        $('input[name="violation_code"][value="other"]').prop('checked', true);
-        var $sel = $('select[name="other_violation_code"]');
-        if ($sel.find('option[value="1003"]').length > 0) {
-          $sel.val('1003');
-        } else if ($sel.find('option[value="2013"]').length > 0) {
-          $sel.val('2013');
-        }
-        GM_addStyle(
-          'input[type="submit"][value="送信"] {padding: 10px 30px;margin: 0 30px;}'
-        );
-        break;
+      });
+      $(document.body).append($('<div>').addClass('violation_report_all').append($button));
+    }
+
+    // その他のページの処理
+    handleOtherPages(pathname) {
+      switch (pathname) {
+        case '/jp/config/violation_report':
+          this.handleReportConfirmationPage();
+          break;
+        case '/jp/show/violation_report':
+          this.handleReportPage();
+          break;
+      }
+    }
+
+    // 通報確認ページの処理
+    handleReportConfirmationPage() {
+      const href = $('a:contains("商品ページに戻る"), a:contains("商品詳細ページに戻る")').attr('href');
+      if (href.match(/\/auction\/(\w+)$/)) {
+        const aid = RegExp.$1;
+        this.violation[aid] = new Date().getTime();
+        this.save();
+      }
+    }
+
+    // 通報ページの処理
+    handleReportPage() {
+      $('html,body').animate({
+        scrollTop: $('form[action$="/config/violation_report"]').offset().top
+      });
+      $('input[name="violation_code"][value="other"]').prop('checked', true);
+      const $sel = $('select[name="other_violation_code"]');
+      if ($sel.find('option[value="1003"]').length > 0) {
+        $sel.val('1003');
+      } else if ($sel.find('option[value="2013"]').length > 0) {
+        $sel.val('2013');
+      }
+      GM_addStyle('input[type="submit"][value="送信"] {padding: 10px 30px;margin: 0 30px;}');
     }
   }
+
+  // 違反通報クラスのインスタンス化
+  new ViolationReport();
 })();
